2ca9b44db96c61091f445dafc367d73baca6de89
vaggogol <vaggogol@gmail.com>
2025-04-10 12:33:31 +0300

    token update final

--- autofill-playwright-server.js
+++ autofill-playwright-server.js
@@ -22,7 +22,7 @@ app.post('/autofill', async (req, res) => {
   console.log('✅ Received payload:', req.body);
 
   console.log('Received token:',token);
-  let sttoken = token.replace(/=$/, '');
+  let sttoken = token.replace(/^=/, '');
   console.log('Sanitized token:', sttoken);
   if (!email || !plate || !birthdate || !license_years || !zip) {
     return res.status(400).json({ success: false, error: 'Missing required fields' });
